f4a44478496726636ed4feeb9f3f2e02b5669638
onehunnitconst <skygana@gmail.com>
2023-10-04 19:20:40 +0900

    navbar

--- danggn/lib/pages/article/article_page.dart
+++ danggn/lib/pages/article/article_page.dart
@@ -85,7 +85,67 @@ class _ArticlePageState extends State<ArticlePage> {
           ),
         ],
       ),
-      bottomSheet: Container(height: 100.0),
+      bottomNavigationBar: Container(
+        padding:
+            EdgeInsets.only(
+              left: 15.0,
+              right: 15.0,
+              bottom: MediaQuery.of(context).viewPadding.bottom,
+            ),
+        decoration: BoxDecoration(
+            border: Border(top: BorderSide(color: Colors.black12))),
+        height: 110.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Icon(CupertinoIcons.heart),
+                VerticalDivider(width: 30, color: Color(0xFFf2f3f7)),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "10,000원",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF212025),
+                      ),
+                    ),
+                    Text(
+                      "가격 제안하기",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFFee7733),
+                        decoration: TextDecoration.underline,
+                        decorationColor: Color(0xFFee7733),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 10.0),
+              decoration: BoxDecoration(
+                color: Color(0xFFee7733),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Text(
+                      "대화 중",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
